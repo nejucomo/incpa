467b919ea10b263ae15bc6bd5b725005d0b7d337
Nate Wilcox <nejucomo+dev@gmail.com>
2025-04-19 08:55:45 -0700

    Add html collapsing to sections many readers may not need to see.

--- incpa/src/byte.rs
+++ incpa/src/byte.rs
@@ -1,8 +1,10 @@
 //! [ByteParser] and other support for parsing bytes
 //!
-//! # TODO:
+//! <details>
+//! <summary>TODO</summary>
 //!
 //! - Replace our concrete `BufferManager` with a trait and impl, because different applications have different performance needs and benefit from different strategies.
+//! </details>
 mod bufmgr;
 mod parser;
 
