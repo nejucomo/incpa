fb925d68cc9a81bfb408f265cf0123620b2c1219
Nate Wilcox <nejucomo+dev@gmail.com>
2025-04-19 08:41:48 -0700

    Add a `TODO` for improving buffer management.

--- incpa/src/byte.rs
+++ incpa/src/byte.rs
@@ -1,4 +1,8 @@
 //! [ByteParser] and other support for parsing bytes
+//!
+//! # TODO:
+//!
+//! - Replace our concrete `BufferManager` with a trait and impl, because different applications have different performance needs and benefit from different strategies.
 mod bufmgr;
 mod parser;
 
