23608168a3531392f9f794b46d7fd22acb3ccd34
nobody <nobody@nowhere.net>
2026-01-21 06:55:30 -0800

    First draft of new trait family for recursive parser f/c states.

--- recursive/src/lib.rs
+++ recursive/src/lib.rs
@@ -1,14 +1,59 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+//! # Design Goals
+//!
+//! - Incrementally parse recursive grammars specified by composition methods.
+//! - Layered crate depending on [incpa].
+//! - Safe rust with `dyn` dispatch.
+//! - [incpa::Parser] composition leverages recursive parser composition; therefore recursive parser components are [incpa::Parser]s.
+//! - [incpa::Parser] compositions work out-of-the-box as recursive parser components, therefore their state has to "lift" continuations as a foreign trait impl.
+#![deny(unsafe_code)]
+
+use incpa::state::{Chomped, Outcome};
+use incpa::{Input, ParserCompose, UniversalParserError};
 
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod tests;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub trait RecursiveParser<I: ?Sized + Input>: ParserCompose {
+    type FState: FeedState<I, Self::Output, Output = Self::Output, Error = Self::Error>;
+}
+
+pub trait FeedState<I: ?Sized + Input, C>: Sized {
+    /// The type of output on successful parse
+    type Output;
+
+    /// The type of errors this parser detects
+    type Error: From<UniversalParserError>;
+
+    type CState: ContinueState<I, C, FState = Self, Output = Self::Output, Error = Self::Error>;
+
+    /// Feed an input reference to the parser to produce an update
+    ///
+    /// Precondition: `input` includes a suffix which has not been seen previously by this parser.
+    #[allow(clippy::type_complexity)]
+    fn feed(
+        self,
+        input: &I,
+    ) -> Result<Chomped<RecOutcome<Self, Self::CState, Self::Output>>, Self::Error>;
+}
+
+pub type RecOutcome<F, C, O> = Outcome<RecState<F, C>, O>;
+
+pub enum RecState<S, C> {
+    State(S),
+    Cont(C),
+}
+
+pub trait ContinueState<I: ?Sized + Input, C>: Sized {
+    /// The type of output on successful parse
+    type Output;
+
+    /// The type of errors this parser detects
+    type Error: From<UniversalParserError>;
+
+    type FState: FeedState<I, C>;
+
+    fn continue_with(
+        self,
+        cval: C,
+    ) -> Result<RecOutcome<Self::FState, Self, Self::Output>, Self::Error>;
 }
---
+++ recursive/src/tests.rs
@@ -0,0 +1,46 @@
+use incpa::Parser;
+use incpa_str::StrParser;
+
+use crate::recursive;
+
+#[derive(Debug)]
+enum Expr {
+    Num(u64),
+    Sym(String),
+    List(Vec<Expr>),
+}
+
+fn expr() -> impl StrParser<Output = Expr> {
+    recursive(expr_rec)
+}
+
+fn expr_rec(rec: ()) -> impl StrParser<Output = Expr> {
+    use Expr::*;
+
+    num().map(Num).or(sym().map(Sym)).or(list(rec).map(List))
+}
+
+fn num() -> impl StrParser<Output = u64> {
+    // Dummy implementation:
+    "42".map(|_| 42)
+}
+
+fn sym() -> impl StrParser<Output = String> {
+    // Dummy implementation:
+    "x".map(|_| "x".to_string())
+}
+
+fn list(rec: ()) -> impl StrParser<Output = Vec<Expr>> {
+    '['.ignore_then(list_tail(rec))
+}
+
+fn list_tail(rec: ()) -> impl StrParser<Output = Vec<Expr>> {
+    ']'.map(|_| vec![])
+        .or(expr_rec(rec))
+        .then_ignore(", ")
+        .then(list_tail(rec))
+        .map(|x, mut xs| {
+            xs.insert(0, x);
+            xs
+        })
+}
