3c6c30433578cf45d3dde62006b6cb95fbcc292d
nobody <nobody@nowhere.net>
2026-01-29 06:30:04 -0800

    Fill out `ParserCompose::either_or` parser impl.

--- state/src/compose/or.rs
+++ state/src/compose/or.rs
@@ -1,21 +1,20 @@
 use incpa_ioe::IncpaIOE;
 
-use crate::map::MapNext as _;
-use crate::{Backtrack, ChompedResult, Outcome, ParserState};
+use crate::compose::EitherOrState;
+use crate::map::{MapNext as _, MapParsed as _};
+use crate::{ChompedResult, Outcome, ParserState};
 
 /// The [ParserState] for parsing `P` then `Q`
 #[derive(Copy, Clone, Debug)]
 pub struct OrState<P, Q> {
-    obp: Option<Backtrack<P>>,
-    q: Q,
+    eost: EitherOrState<P, Q>,
 }
 
 impl<P, Q> OrState<P, Q> {
     /// Construct a new [OrState]
     pub fn new(p: P, q: Q) -> Self {
         OrState {
-            obp: Some(Backtrack::new(p)),
-            q,
+            eost: EitherOrState::new(p, q),
         }
     }
 }
@@ -36,30 +35,13 @@ where
     Q: ParserState<Input = P::Input, Output = P::Output, Error = P::Error>,
 {
     fn feed(self, input: &Self::Input) -> ChompedResult<Outcome<Self, Self::Output>, Self::Error> {
-        let OrState { obp, q } = self;
-
-        if let Some(bp) = obp {
-            let res = bp.feed(input);
-            if res.is_ok() {
-                return res.map_next(|bp| OrState { obp: Some(bp), q });
-            }
-            // Else we hit an error, so drop `bp` and fall back to `q`:
-        }
-
-        q.feed(input).map_next(|q| OrState { obp: None, q })
+        self.eost
+            .feed(input)
+            .map_next(|eost| OrState { eost })
+            .map_parsed(|ei| ei.into_inner())
     }
 
     fn end_input(self, final_input: &Self::Input) -> Result<Self::Output, Self::Error> {
-        let OrState { obp, q } = self;
-
-        if let Some(bp) = obp {
-            let res = bp.end_input(final_input);
-            if res.is_ok() {
-                return res;
-            }
-            // Else we hit an error, so drop `bp` and fall back to `q`:
-        }
-
-        q.end_input(final_input)
+        self.eost.end_input(final_input).map(|ei| ei.into_inner())
     }
 }
